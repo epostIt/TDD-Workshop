669309426b4e853fdcae99d07397aa2133d72809
Holaday <tristan.holaday@tholaday-mac.local>
2022-02-07 09:10:17 -0700

    added another mocking test.

--- src/TDD_Practice_Examples/MockApi/MockApi.spec.ts
+++ src/TDD_Practice_Examples/MockApi/MockApi.spec.ts
@@ -1,1 +1,3 @@
+// Write a test that mocks the data given back by getLukeAPI to be {name: 'Leia'}
 
+// Write a test that asserts that callMe() gets called 1 times
--- src/TDD_Practice_Examples/MockApi/MockApi.ts
+++ src/TDD_Practice_Examples/MockApi/MockApi.ts
@@ -3,3 +3,11 @@ import { getLukeAPI } from "./SWAPI";
 export const getLuke = () => {
   return getLukeAPI();
 };
+
+export const callMe = () => {
+  return "this is a function";
+};
+
+export const callingCallMe = () => {
+  return callMe();
+};
