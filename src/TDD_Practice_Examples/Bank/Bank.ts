1a3789bf41c95490e649dba487e7a8c3edea9933
Elisabeth Post <elisabeth.post@centauricorp.com>
2022-07-18 19:39:29 -0600

    add Banking Kata

---
+++ src/TDD_Practice_Examples/Bank/Bank.ts
@@ -0,0 +1,8 @@
+//You have been asked to create a banking application
+//The bank can keep track of how much money it has, and how much it has loaned.
+//Customers have a saving account, checking account, and need to be able to transfer money between the two.
+//Customers need to be able to deposit and withdraw money
+//Customers can also take out loans from the bank, and need to be able to view them
+//The interest rate starts at 2.5% and doubles for every additional loan a customer takes out (but the other loans stay the same: loan 1 2.5%, loan 2 is 5%, etc)
+//Robbers can come in an rob a customer at random (empty their accounts)
+//Robinhood can come in and relieve a customer of their loans at random
